6c97b8f3891387f025aeafabda0bb5d786f5b000
vivim212 <vivimai@live.com>
2025-05-26 15:43:20 +0200

    Update Drehgeber_neu.ino
    Drehgeber Positionsmessung

--- Drehgeber_neu/Drehgeber_neu.ino
+++ Drehgeber_neu/Drehgeber_neu.ino
@@ -1,33 +1,18 @@
-#define encoderPinA 2
-#define encoderPinB 3
+#define encoderPinA 2 // Pin für Phase A
+#define encoderPinB 3 // Pin für Phase B
+volatile long encoderPos = 0;
+// Anpassung des mm-Werts: Der Wert mmPerPulse muss basierend auf der Mechanik (z.B. Übersetzung, Radgröße) angepasst werden
+float mmPerPulse = 0.000104; //bei 2400 Umdrehungen
 
-volatile long encoderTicks = 0;
-
-void setup() {
+void setup() 
+{
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(encoderPinA), handleEncoderA, RISING);
-
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), readEncoder, CHANGE);
   Serial.begin(9600);
+
 }
 
-<<<<<<< HEAD
-void loop() {
-  static long lastTicks = 0;
-  if (encoderTicks != lastTicks) {
-    Serial.print("Encoder-Position: ");
-    Serial.println(encoderTicks);
-    lastTicks = encoderTicks;
-  }
-}
-
-void handleEncoderA() {
-  if (digitalRead(encoderPinB) == LOW) {
-    encoderTicks++;
-  } else {
-    encoderTicks--;
-=======
 void loop() 
 {
   readEncoder();
@@ -49,6 +34,11 @@ void readEncoder()
   if(xorResult)
   {
     encoderPos++;
->>>>>>> b91b26d7eafe8a01b79cf770f08c5975dbfc1f9f
   }
+  else
+  {
+    encoderPos--;
+  }
+
+
 }
